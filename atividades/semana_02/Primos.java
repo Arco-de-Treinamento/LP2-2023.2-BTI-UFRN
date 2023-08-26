f2382e5e6ef3dbee9667d41c4c94165c05de2516
Manoel Freitas <josmanoel.freitas@gmail.com>
2023-08-26 17:29:15 -0300

    Semana 02

---
+++ atividades/semana_02/Primos.java
@@ -0,0 +1,38 @@
+public class Primos {
+
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Você deve passar um parâmetro.");
+            System.exit(0);
+        }
+
+        int qnt = Integer.parseInt(args[0]); //Converte para número a entrada
+        
+        int[] primos = new int[qnt];
+        primos[0] = 2;
+
+        int count = 1;
+        int num = 3;
+
+        while (count < qnt) {
+            boolean isPrimo = true;
+
+            for (int j = 0; j < count; j++) {
+                if (num % primos[j] == 0) {
+                    isPrimo = false;
+                    break;
+                }
+            }
+            if (isPrimo) {
+                primos[count] = num;
+                count++;
+            }
+            num++;
+        }
+
+        for (int primo : primos) {
+            System.out.println(primo);
+        }
+    }
+
+}
